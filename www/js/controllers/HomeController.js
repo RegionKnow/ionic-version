02582f42aca28f81db69278cd30df58cb36f6e16
lentusignavus <ignavus.ii@gmail.com>
2015-09-29 13:28:58 -0700

    Modular controllers add

---
+++ www/js/controllers/HomeController.js
@@ -0,0 +1,8 @@
+(function() {
+  angular.module('regiknow').controller("HomeController", HomeController);
+  HomeController.$inject = [];
+  function HomeController(){
+var vm = this;
+vm.sample = "Hello";
+  }
+})();
