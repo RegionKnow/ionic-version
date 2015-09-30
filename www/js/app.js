493c704c3c26007df28741a1d429f737effd9900
lentusignavus <ignavus.ii@gmail.com>
2015-09-30 15:55:00 -0700

    Side nav with material design working

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('regiknow', ['ionic'])
+angular.module('regiknow', ['ionic', 'ngMaterial'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -18,4 +18,8 @@ angular.module('regiknow', ['ionic'])
   })
 
 
-})
+}).config(function($mdThemingProvider) {
+  $mdThemingProvider.theme('default')
+    .primaryPalette('red')
+    .accentPalette('indigo');
+});
--- www/js/controllers/HomeController.js
+++ www/js/controllers/HomeController.js
@@ -1,8 +1,31 @@
 (function() {
   angular.module('regiknow').controller("HomeController", HomeController);
-  HomeController.$inject = [];
-  function HomeController(){
-var vm = this;
-vm.sample = "Hello";
+  HomeController.$inject = ['$scope', '$timeout', '$mdSidenav', '$mdUtil'];
+
+  function HomeController($scope, $timeout, $mdSidenav, $mdUtil) {
+    var vm = this;
+    vm.sample = "Hello";
+    vm.toggleLeft = buildToggler('left');
+
+    function buildToggler(navID) {
+      var debounceFn = $mdUtil.debounce(function() {
+        $mdSidenav(navID)
+          .toggle()
+          .then(function() {
+            console.log("toggle " + navID + " is done");
+          });
+      }, 200);
+      return debounceFn;
+    }
+
+    vm.close = function() {
+      $mdSidenav('left').close()
+        .then(function() {
+          $log.debug("close LEFT is done");
+        });
+    }
+
+
+
   }
 })();
