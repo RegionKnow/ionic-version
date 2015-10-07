acb3c61932ee4e9cfd1f035427f4447dcaf36ab5
kandianne <kandipierre@gmail.com>
2015-10-06 17:28:20 -0700

    fixed status reflection when loggedin or out

--- www/js/controllers/UserController.js
+++ www/js/controllers/UserController.js
@@ -1,13 +1,14 @@
 (function() {
 	angular.module('regiknow').controller("UserController", UserController);
-	UserController.$inject = ['UserFactory', '$state', '$rootScope', 'ionicMaterialInk'];
-	function UserController(UserFactory, $state, $rootScope, ionicMaterialInk){
+	UserController.$inject = ['UserFactory', '$state', '$rootScope', 'ionicMaterialInk', '$ionicSideMenuDelegate'];
+	function UserController(UserFactory, $state, $rootScope, ionicMaterialInk, $ionicSideMenuDelegate){
 		var vm = this;
 		vm.status = $rootScope._user;
-
+		console.log(vm.status);
 
 		ionicMaterialInk.displayEffect();
 
+		vm.welcome = true;
 
 		//---------FUNCTIONALITY FORa REGISTER & LOGIN USER----------------------------------------------------------
 
@@ -33,5 +34,93 @@
 			});
 		};
 
-	}
+
+
+
+      //MD Side Nav=======================================================================================================
+      vm.toggleLeft = function() {
+      	$ionicSideMenuDelegate.toggleLeft();
+      };
+
+
+      function buildToggler(navID) {
+      	var debounceFn = $mdUtil.debounce(function() {
+      		$mdSidenav(navID)
+      		.toggle()
+      		.then(function() {
+      			console.log("toggle " + navID + " is done");
+      		});
+      	}, 200);
+      	return debounceFn;
+      }
+
+      //FOR STATES TO SHOW AS TITLE=====================================
+
+      vm.trueUserSettings = function() {
+      	vm.userSettings = true;
+      	vm.welcome = false;
+      	vm.login = false;
+      	vm.profile = false;
+      	vm.questions = false;
+      	vm.questionsFeed = false;
+
+      };
+
+      vm.trueLogin = function() {
+      	vm.userSettings = false;
+      	vm.welcome = false;
+      	vm.login = true;
+      	vm.profile = false;
+      	vm.questions = false;
+      	vm.questionsFeed = false;
+
+      };
+
+      vm.trueWelcome = function() {
+      	vm.userSettings = false;
+      	vm.welcome = true;
+      	vm.login = false;
+      	vm.profile = false;
+      	vm.questions = false;
+      	vm.questionsFeed = false;
+
+      };
+
+      vm.trueProfile = function() {
+      	vm.userSettings = false;
+      	vm.welcome = false;
+      	vm.login = false;
+      	vm.profile = true;
+      	vm.questions = false;
+      	vm.questionsFeed = false;
+      };
+
+      vm.trueQuestions = function() {
+      	vm.userSettings = false;
+      	vm.welcome = false;
+      	vm.login = false;
+      	vm.profile = false;
+      	vm.questions = true;
+      	vm.questionsFeed = false;
+      };
+
+      vm.trueQsFeed = function() {
+      	vm.userSettings = false;
+      	vm.welcome = false;
+      	vm.login = false;
+      	vm.profile = false;
+      	vm.questions = false;
+      	vm.questionsFeed = true;
+      }
+
+      
+      // vm.close = function() {
+      //   $mdSidenav('left').close()
+      //   .then(function() {
+      //     $log.debug("close LEFT is done");
+      //   });
+      // }
+
+
+  }
 })();
