3f6208408710b5fd0ee355a90fb39300109ef3d5
kandianne <kandipierre@gmail.com>
2015-10-21 00:13:20 -0700

    add action button

---
+++ www/js/controllers/FabController.js
@@ -0,0 +1,92 @@
+(function(){
+	'use strict'
+	angular.module('regiknow').controller('FabController', FabController);
+	FabController.$inject = [];
+
+	function FabController(){
+
+		var vm = this;
+		
+		vm.todos = [
+		{
+			face : 'http://lorempixel.com/50/50/people',
+			what: 'Brunch this weekend?',
+			who: 'Min Li Chan',
+			when: '3:08PM',
+			notes: " I'll be in your neighborhood doing errands"
+		},
+		{
+			face : 'http://lorempixel.com/50/50/people',
+			what: 'Brunch this weekend?',
+			who: 'Min Li Chan',
+			when: '3:08PM',
+			notes: " I'll be in your neighborhood doing errands"
+		},
+		{
+			face : 'http://lorempixel.com/50/50/people',
+			what: 'Brunch this weekend?',
+			who: 'Min Li Chan',
+			when: '3:08PM',
+			notes: " I'll be in your neighborhood doing errands"
+		},
+		{
+			face : 'http://lorempixel.com/50/50/people',
+			what: 'Brunch this weekend?',
+			who: 'Min Li Chan',
+			when: '3:08PM',
+			notes: " I'll be in your neighborhood doing errands"
+		},
+		{
+			face : 'http://lorempixel.com/50/50/people',
+			what: 'Brunch this weekend?',
+			who: 'Min Li Chan',
+			when: '3:08PM',
+			notes: " I'll be in your neighborhood doing errands"
+		},
+		{
+			face : 'http://lorempixel.com/50/50/people',
+			what: 'Brunch this weekend?',
+			who: 'Min Li Chan',
+			when: '3:08PM',
+			notes: " I'll be in your neighborhood doing errands"
+		},
+		{
+			face : 'http://lorempixel.com/50/50/people',
+			what: 'Brunch this weekend?',
+			who: 'Min Li Chan',
+			when: '3:08PM',
+			notes: " I'll be in your neighborhood doing errands"
+		},
+		{
+			face : 'http://lorempixel.com/50/50/people',
+			what: 'Brunch this weekend?',
+			who: 'Min Li Chan',
+			when: '3:08PM',
+			notes: " I'll be in your neighborhood doing errands"
+		},
+		{
+			face : 'http://lorempixel.com/50/50/people',
+			what: 'Brunch this weekend?',
+			who: 'Min Li Chan',
+			when: '3:08PM',
+			notes: " I'll be in your neighborhood doing errands"
+		},
+		{
+			face : 'http://lorempixel.com/50/50/people',
+			what: 'Brunch this weekend?',
+			who: 'Min Li Chan',
+			when: '3:08PM',
+			notes: " I'll be in your neighborhood doing errands"
+		},
+		{
+			face : 'http://lorempixel.com/50/50/people',
+			what: 'Brunch this weekend?',
+			who: 'Min Li Chan',
+			when: '3:08PM',
+			notes: " I'll be in your neighborhood doing errands"
+		}
+		];
+
+	}
+
+})();
